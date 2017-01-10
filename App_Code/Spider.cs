1866f95636335f0cd9fd8d300f0b8a7a5cac493d
cangj <cangj@DESKTOP-DI76L4K>
2017-01-10 14:23:46 +0800

    import util lib

---
+++ App_Code/Spider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Spider
+/// </summary>
+public class Spider
+{
+    public Spider()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
